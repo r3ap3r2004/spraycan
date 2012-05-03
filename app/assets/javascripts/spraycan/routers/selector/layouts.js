668b01c07b441e89d75de5e3ad6bbda137484e63
Andrew Hooker <GeekOnCoffee@gmail.com>
2012-05-03 09:41:38 -0500

    Adding Routes to Allow Deep Linking

--- app/assets/javascripts/spraycan/routers/selector/layouts.js
+++ app/assets/javascripts/spraycan/routers/selector/layouts.js
@@ -1,6 +1,9 @@
 Spraycan.Routers.Layouts = Backbone.Router.extend({
   routes: {
     "tab-layouts": "index",
+	"tab-layouts-product-detail": "index",
+	"tab-layouts-product-listing": "index",
+	"tab-layouts-cart": "index",
   },
 
   index: function(){
